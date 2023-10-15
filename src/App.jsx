a9496d7fde5eb9c9f3ed81e2cbfda58447410609
lengoctramanh <letramanh2182005@gmail.com>
2023-10-15 21:45:57 +0700

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Fragment, useEffect, useRef, useState } from "react";
 import Test1 from "./Test1";
-import Test2 from "../Test2";
+import Test2 from "./Test2";
 import Test3 from "./Test3";
 import Test4 from "./Test4";
 const App = () => {
